3854b0de3681a6ff68b8361d82b605d0459a8b8f
Kahani22 <43250430+Kahani22@users.noreply.github.com>
2019-12-17 12:37:03 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
     host: "us-cdbr-iron-east-05.cleardb.net",
-    port: 3306, // 8889 for mac
+    // port: 3306, // 8889 for mac
     uname: "bf8a8ca10780ed",
     pword: "068d99e3", // leave blank for windows
     database: "heroku_aabd80cc426044b"
